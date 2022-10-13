bc2393c7ac0824f90767855b967b842688c636c7
hudahai <hscxrzs@gmail.com>
2022-10-13 14:42:46 +0800

    feat: review sort.Search

--- median_sliding_window/median_sliding_window_test.go
+++ median_sliding_window/median_sliding_window_test.go
@@ -2,9 +2,15 @@ package median_sliding_window
 
 import (
 	"fmt"
+	"sort"
 	"testing"
 )
 
 func Test_medianSlidingWindow(t *testing.T) {
 	fmt.Println(medianSlidingWindow([]int{1, 2, 3, 4}, 3))
+}
+
+func Test_search(t *testing.T) {
+	a := []int{1, 3, 5, 10}
+	fmt.Println(sort.Search(len(a), func(i int) bool { return 11 <= a[i] }))
 }
