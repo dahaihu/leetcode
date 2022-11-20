3277395dc7a7ba9325f60bcc6ff605022bb8ed4b
hudahai <hscxrzs@gmail.com>
2022-11-20 14:29:36 +0800

    feat: num is ilands

--- num_islands/num_islands.go
+++ num_islands/num_islands.go
@@ -1,8 +1,26 @@
 package num_islands
 
 func dfs(grid [][]byte, i, j int) {
-
+	if i < 0 || i >= len(grid) ||
+		j < 0 || j >= len(grid[0]) ||
+		grid[i][j] == '0' {
+		return
+	}
+	grid[i][j] = '0'
+	dfs(grid, i-1, j)
+	dfs(grid, i+1, j)
+	dfs(grid, i, j-1)
+	dfs(grid, i, j+1)
 }
 func numIslands(grid [][]byte) int {
-	return 0
+	count := 0
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[0]); j++ {
+			if grid[i][j] == '1' {
+				count++
+				dfs(grid, i, j)
+			}
+		}
+	}
+	return count
 }
---
+++ num_islands/num_islands_test.go
@@ -0,0 +1,10 @@
+package num_islands
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_numIsLands(t *testing.T) {
+	fmt.Println(numIslands([][]byte{{'1', '0', '1'}, {'0', '0', '0'}, {'1', '0', '1'}}))
+}
