60cbd9813c34ace98573611059f916dfe42a5cd7
hudahai <hscxrzs@gmail.com>
2022-11-29 09:30:57 +0800

    feat: review can partition

--- can_partition/can_partition.go
+++ can_partition/can_partition.go
@@ -8,20 +8,24 @@ func canPartition(nums []int) bool {
 	if sum%2 == 1 {
 		return false
 	}
+
 	mark := make([][]bool, sum/2+1)
 	for i := 0; i < sum/2+1; i++ {
 		mark[i] = make([]bool, len(nums)+1)
 	}
 	mark[0][0] = true
+
 	for i := 1; i < sum/2+1; i++ {
 		for j := 1; j < len(nums)+1; j++ {
 			if i < nums[j-1] {
-				continue
+				break
 			}
+			// candidate: nums[j-1]
+			// target: i
 			mark[i][j] = mark[i-nums[j-1]][j-1]
 			if mark[i][j] {
-				for j1 := j; j1 < len(nums)+1; j1++ {
-					mark[i][j1] = true
+				for jj := j + 1; jj < len(nums)+1; jj++ {
+					mark[i][jj] = true
 				}
 				break
 			}
