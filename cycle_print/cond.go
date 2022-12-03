acae4fb4a72b9229cea00ebbf6e6a3fbcd0277a8
hudahai <hscxrzs@gmail.com>
2022-12-03 13:53:15 +0800

    feat: review cond cycle print

--- cycle_print/cond.go
+++ cycle_print/cond.go
@@ -6,23 +6,29 @@ import (
 )
 
 func CondCyclePrint(workers int, target int) {
+	// cond.Wait()
+	// cond.Signal()
+	// cond.Broadcast()
+	// cond.L.Lock()
+	// cond.L.UnLock()
 	cond := sync.NewCond(&sync.Mutex{})
 	var (
 		val int
 		wg  sync.WaitGroup
 	)
+	wg.Add(workers)
 	for i := 0; i < workers; i++ {
-		wg.Add(1)
 		i := i
 		go func() {
 			defer wg.Done()
+
 			loop := true
 			for loop {
 				cond.L.Lock()
 				for val%workers != i {
 					cond.Wait()
 				}
-				if val < target {
+				if val <= target {
 					fmt.Printf("worker %d print %d\n", i, val)
 				} else {
 					loop = false
