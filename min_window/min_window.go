a2234df8dc920f6a9712299a0b90625b4bda78d7
hushichang <hushichang@sensetime.com>
2023-01-08 21:39:13 +0800

    feat: min window

--- min_window/min_window.go
+++ min_window/min_window.go
@@ -12,49 +12,46 @@ func minWindow(s string, t string) string {
 		miss[b]++
 	}
 	has := make(map[byte]int)
-	nextStart := func(start, end int) int {
-		for j := start; j <= end; j++ {
-			jb := s[j]
-			if _, ok := need[jb]; !ok {
-				continue
-			}
-			hc, ec := has[jb], need[jb]
-			if hc > ec {
-				has[jb] = hc - 1
-			} else {
-				miss[jb] = 1
-				has[jb] = hc - 1
-				if curLen := end - j + 1; curLen < len(result) {
-					result = s[j : end+1]
-				}
-				return j + 1
-			}
-		}
-		panic("invalid input")
-	}
 	for i := 0; i < len(s); i++ {
 		b := s[i]
-		if _, needed := need[b]; !needed {
+		if _, needok := need[b]; !needok {
 			continue
 		}
+		has[b]++
 		if start == -1 {
 			start = i
 		}
-		has[b]++
-		missc, missed := miss[b]
-		if !missed {
+		missc, missok := miss[b]
+		if !missok {
 			continue
 		}
 		if missc > 1 {
 			miss[b] = missc - 1
 		} else {
 			delete(miss, b)
-			if len(miss) == 0 {
-				start = nextStart(start, i)
+			if len(miss) != 0 {
+				continue
+			}
+			for j := start; j <= i; j++ {
+				jb := s[j]
+				if _, needok := need[jb]; !needok {
+					continue
+				}
+				hasc, needc := has[jb], need[jb]
+				if hasc > needc {
+					has[jb] = hasc - 1
+				} else {
+					has[jb] = hasc - 1
+					miss[jb] = 1
+					start = j + 1
+					if curLen := i - j + 1; curLen < len(result) {
+						result = s[j : i+1]
+					}
+					break
+				}
 			}
 		}
 	}
-
 	if len(result) == len(s)+1 {
 		return ""
 	}
