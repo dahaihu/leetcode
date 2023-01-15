afce04c885a2d135e5e94ee71700c0a681ff9d6f
hushichang <hushichang@sensetime.com>
2023-01-15 12:00:35 +0800

    feat: subarray sum review

--- subarray_sum/subarray_num.go
+++ subarray_sum/subarray_num.go
@@ -2,16 +2,17 @@ package subarray_sum
 
 func subarraySum(nums []int, target int) [][]int {
 	mark := make(map[int][]int)
+	// cur - pre = target -> pre = cur - target
 	mark[0] = []int{-1}
 	var (
-		result [][]int
 		sum    int
+		result [][]int
 	)
 	for idx, num := range nums {
 		sum += num
-		if suiteds, ok := mark[sum-target]; ok {
-			for _, suited := range suiteds {
-				result = append(result, nums[suited+1:idx+1])
+		if preEnds := mark[sum-target]; len(preEnds) > 0 {
+			for _, preEnd := range preEnds {
+				result = append(result, nums[preEnd+1:idx+1])
 			}
 		}
 		mark[sum] = append(mark[sum], idx)
